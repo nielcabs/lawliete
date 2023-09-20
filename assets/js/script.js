051d63845e359598b71cab58343ec96cb4cd5ff3
Niel <82821463+nielcabs@users.noreply.github.com>
2023-09-20 09:03:02 +0800

    Latest

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * navbar variables
+ */
+
+const navbar = document.querySelector("[data-nav]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+    elemToggleFunc(document.body);
+  })
+
+}
+
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+
+})
+
+function scrollToWinners() {
+  const winnersSection = document.getElementById("winnerpart");
+  if (winnersSection) {
+    winnersSection.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+// Add an event listener to your button or link
+const winnersButton = document.getElementById("winnersButton"); // Replace "winnersButton" with the actual ID of your button or link
+if (winnersButton) {
+  winnersButton.addEventListener("click", scrollToWinners);
+}
